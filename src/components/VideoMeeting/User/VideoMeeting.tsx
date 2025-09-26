a408ea70c89865eac6181d4d60678ea0e8e2e14d
Viktor <viktor@royallegalsolutions.com>
2025-09-26 07:55:29 +0300

    made chatbox and participants panel collapsed as default

--- src/components/VideoMeeting/User/VideoMeeting.tsx
+++ src/components/VideoMeeting/User/VideoMeeting.tsx
@@ -6,17 +6,17 @@ import { UserMeeting } from "./UserMeeting";
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "../../ui/select";
 
 export const VideoMeeting = () => {
- 
+
   return (
-    <div className="flex flex-1 flex-col p-4">
-      <div className="flex justify-between bg-white p-6 rounded-lg border border-royal-light-gray mb-3">
+    <div className="h-screen w-screen flex flex-col overflow-hidden">
+      <div className="flex justify-between bg-white p-6 rounded-lg border border-royal-light-gray mb-3 flex-shrink-0">
         <div className="flex gap-4 items-center">
           <BoxSelectIcon className="h-10 w-10 text-royal-gray" />
           <h1 className="text-2xl font-bold text-royal-dark-gray mb-2 uppercase">Elite Coaching 09-30-25</h1>
         </div>
         <div className="flex items-center gap-2">
           <p className="mr-5">0&nbsp;Viewers&nbsp;Attended</p>
-          <UserIcon className="w-10"/>
+          <UserIcon className="w-10" />
           <Select>
             <SelectTrigger className="border-royal-light-gray">
               <SelectValue placeholder={'Choose an Option...'} />
@@ -27,17 +27,11 @@ export const VideoMeeting = () => {
           </Select>
         </div>
       </div>
-      <div className="flex flex-1">
-        {/* Sidebar */}
-        <div className="flex flex-col gap-3 w-48 p-4 border-r">
-          <h2>Calendars</h2>
-          <Button>Discovery</Button>
-          <Button>Consult</Button>
-        </div>
+      <div className="flex flex-1 min-h-0">
 
         {/* Video / Room Area */}
-        <div className="flex-1 p-4 flex flex-col">
-          <UserMeeting/>
+        <div className="flex-1 p-4 flex flex-col min-h-0">
+          <UserMeeting />
         </div>
 
       </div>

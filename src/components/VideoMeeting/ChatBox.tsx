53bbd548169798bf5a5700168523fa6fa2a6e230
dev_alexandr <dev_alexandr@users.noreply.github.com>
2025-10-24 00:29:46 +0300

    fixed avatar

--- src/components/VideoMeeting/ChatBox.tsx
+++ src/components/VideoMeeting/ChatBox.tsx
@@ -65,7 +65,7 @@ const getUserInitials = (username: string): string => {
   if (words.length === 1) {
     return words[0].substring(0, 2).toUpperCase();
   }
-  return (words[0][0] + words[words.length - 1][0]).toUpperCase();
+  return (words[0][0]);
 };
 
 interface ChatBoxProps {

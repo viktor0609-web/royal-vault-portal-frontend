12a53707cb6b8b77c38ab586d019d6dc52a3ee14
Viktor <viktor@royallegalsolutions.com>
2025-11-25 22:08:30 +0200

    aligned in the sidebar

--- src/components/user/RoyalVaultSidebar.tsx
+++ src/components/user/RoyalVaultSidebar.tsx
@@ -112,7 +112,7 @@ export function RoyalVaultSidebar() {
           </SidebarGroup>
         )}
 
-        <div className="flex-1 flex items-center justify-center">
+        <div className="flex-1 flex items-start pt-4">
           <SidebarGroup>
             <SidebarGroupContent>
               <SidebarMenu className="space-y-1">
